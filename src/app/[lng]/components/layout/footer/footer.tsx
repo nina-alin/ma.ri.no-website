d7ead5162bd83fd06a15b485dfbc5ac44f9aa4af
Nina Alin <alin.nina28@gmail.com>
2023-08-24 00:05:23 +0200

    feat: translate footer and add some styling

--- src/app/[lng]/components/layout/footer/footer.tsx
+++ src/app/[lng]/components/layout/footer/footer.tsx
@@ -4,10 +4,13 @@ import MapLinks from "@/app/[lng]/components/layout/map-links";
 import MapProjects from "@/app/[lng]/components/layout/map-projects";
 import Logo from "@/app/[lng]/components/svg/logo";
 import Instagram from "@/app/components/svg/instagram";
+import { translate } from "@/app/i18n";
 
 import styles from "./footer.module.css";
 
-const Footer = ({ lng }: { lng: string }) => {
+const Footer = async ({ lng }: { lng: string }) => {
+  const { t } = await translate(lng);
+
   return (
     <footer className={styles.footer} data-scroll-section>
       <div className={styles.section}>
@@ -15,24 +18,26 @@ const Footer = ({ lng }: { lng: string }) => {
           <div className={styles.logo}>
             <Logo />
           </div>
-          <p>ma.ri.no est une auto-entreprise spécialisée dans le graphisme.</p>
+          <p>{t("footer.description")}</p>
         </div>
         <div className={styles.linksContainer}>
           <div className={styles.links}>
-            <h4>Menu</h4>
+            <h4>{t("footer.menu")}</h4>
             <ul>
               <MapLinks lng={lng} />
-              <Link href={"/projects"}>projects</Link>
+              <Link href={"/projects"}>
+                {t("footer.projects").toLowerCase()}
+              </Link>
             </ul>
           </div>
           <div className={styles.links}>
-            <h4>Projects</h4>
+            <h4>{t("footer.projects")}</h4>
             <ul>
               <MapProjects lng={lng} />
             </ul>
           </div>
           <div className={styles.links}>
-            <h4>Contact</h4>
+            <h4>{t("footer.contact")}</h4>
             <ul>
               <li>
                 <a href={"tel:+000"}>+0000000</a>
@@ -48,13 +53,24 @@ const Footer = ({ lng }: { lng: string }) => {
       <div className={styles.sectionBottom}>
         <div className={styles.credits}>
           <p>© 2023 ma.ri.no</p>
-          <p>Design by Déborah Marino and Nina Alin</p>
-          <p>Developed by Nina Alin</p>
-        </div>
-        <div className={styles.legal}>
-          <p>Mentions légales</p>
-          <p>CGU</p>
-          <p>CGV</p>
+          <p>
+            {t("footer.designedBy")}
+            <Link
+              style={{ textDecoration: "underline" }}
+              href={"https://github.com/nina-alin"}
+            >
+              Nina Alin
+            </Link>
+          </p>
+          <p>
+            {t("footer.developedBy")}{" "}
+            <Link
+              style={{ textDecoration: "underline" }}
+              href={"https://github.com/nina-alin"}
+            >
+              Nina Alin
+            </Link>
+          </p>
         </div>
         <Link href={"https://www.instagram.com/ma.ri.no_studio/"}>
           <div className={styles.socials}>
