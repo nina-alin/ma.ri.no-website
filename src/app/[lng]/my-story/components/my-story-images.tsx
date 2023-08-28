1be47c64492c5760d548519033be1224cc9b47c1
Nina Alin <alin.nina28@gmail.com>
2023-08-28 14:44:56 +0200

    feat: my story page

---
+++ src/app/[lng]/my-story/components/my-story-images.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { ImageProperties } from "@/app/[lng]/my-story/page";
+import Modal from "@/app/components/modal/modal";
+
+import styles from "../page.module.css";
+
+const MyStoryImages = ({ image }: { image: ImageProperties }) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <div className={styles.imageText}>
+        <Image
+          onClick={() => setOpenModal(true)}
+          src={image.src}
+          alt={image.alt}
+          width={5000}
+          height={5000}
+        />
+        <p data-scroll data-scroll-speed={"1"}>
+          {image.translation.toUpperCase()}
+        </p>
+      </div>
+      {openModal && (
+        <Modal open={openModal}>
+          <h1>modal</h1>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default MyStoryImages;
---
+++ src/app/[lng]/my-story/page.tsx
@@ -0,0 +1,68 @@
+import MyStoryImages from "@/app/[lng]/my-story/components/my-story-images";
+import { translate } from "@/app/i18n";
+
+import asahikawa from "../../../../public/asahikawa.jpg";
+import brussels from "../../../../public/brussels.jpg";
+import catania from "../../../../public/catania.jpg";
+import lille from "../../../../public/lille.jpg";
+import styles from "./page.module.css";
+
+export type ImageProperties = {
+  src: string;
+  alt: string;
+  translation: string;
+};
+const MyStoryPage = async ({
+  params: { lng },
+}: {
+  params: {
+    lng: string;
+  };
+}) => {
+  const { t } = await translate(lng);
+
+  const imagesFirstRow: ImageProperties[] = [
+    {
+      src: lille,
+      alt: "lille",
+      translation: t("my-story.lille"),
+    },
+    {
+      src: brussels,
+      alt: "brussels",
+      translation: t("my-story.brussels"),
+    },
+  ];
+
+  const imagesSecondRow: ImageProperties[] = [
+    {
+      src: catania,
+      alt: "catania",
+      translation: t("my-story.catania"),
+    },
+    {
+      src: asahikawa,
+      alt: "asahikawa",
+      translation: t("my-story.asahikawa"),
+    },
+  ];
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.imagesContainer}>
+        <div className={styles.images}>
+          {imagesFirstRow.map((image) => (
+            <MyStoryImages key={image.alt} image={image} />
+          ))}
+        </div>
+        <div className={styles.images}>
+          {imagesSecondRow.map((image) => (
+            <MyStoryImages key={image.alt} image={image} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyStoryPage;
